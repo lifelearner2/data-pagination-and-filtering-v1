9c66ab75aeb81d7c7e26d93800e54b1a3c6bdfac
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-06-06 13:47:32 -0700

    Began Code
    for project Unit 2

--- js/script.js
+++ js/script.js
@@ -17,6 +17,20 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page) {
+  const startIndex = (page * 9) - 9;
+  const endIndex = (page * 9);
+  const studentList = document.getElementsByClassName("student-list");
+  
+  studentList.innerHTML = ('');
+   for ( let i = 0; i < list.length; i++) {
+      if (i >= startIndex, i < endIndex);
+   }
+};
+     
+   
+  
+  
 
 
 
