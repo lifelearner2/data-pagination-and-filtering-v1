6367267f5521c140a2fd643e914dc6988fb1091e
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-06-07 13:26:58 -0700

    Update script.js
    updating function

--- js/script.js
+++ js/script.js
@@ -17,57 +17,46 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+//I created a function called showPage and the variables startIndex for the index of the first student and endIndex for the last student on the page.
+//
+
 function showPage(list, page) {
-  const startIndex = (page * 9) - 9;
-  const endIndex = (page * 9);
-  const studentList = document.getElementsByClassName("student-list");
-  
-  studentList.innerHTML = ('');
-   for ( let i = 0; i < list.length; i++) {
-      if (i >= startIndex && i < endIndex) {
-            ` <li class="student-item cf">
-            <div class="student-details">
-            <img class="avatar" src= ${"picture"} ${alt="Profile Picture"}>
-            <h3>${name}</h3>
-            <span class=${"email"}></span>
-            </div>
-            <div class=${"joined-details"}>
-            <span class=${"date"}>Joined</span>
-            </div>
-         </li> `
-            // insertAdjacentHTML beforeend 
-      
-      
-      };
+      const startIndex = (page * 9) - 9;
+      const endIndex = (page * 9);
+      const studentList = document.querySelectorAll(student-list);
+      //return showPage;
+     //studentList.innerHTML = ('studentItem');  
+     studentList.innerHTML = (''); 
 
-      //   li ="student-item cf";
-      //   let studentItem ="student-details" (`img class="avatar" src="js.data.js" alt="Profile, Picture`)
-      //   let h3 = "Name";
-      //   span_class="email";
-      //   div = "data";
-      //   div_class="joined-details";
-      //   span_lass="date";
-        //div
-        //li
-
-         showPage[data];
- 
+       for ( let i = 0; i < list.length; i++) {
+            if (i >= startIndex && i < endIndex) {
+               const studentItem = 
+               ` <li class="student-item cf[i].name.first">
+                  <div class="student-details [i].name.details">
+                  <img class="avatar" src= "picture" alt="Profile Picture">
+                  <h3>list[i]name.first</h3>
+                  <span class="data[i]email"></span>
+                  </div>
+                  <div class="data[i]joined.details">
+                  <span class="data[i]joined.date"</span>
+                  </div>
+               </li> `
+                 
+                 insertAdjacentHTML(beforeend, studentItem);
+                 showPage(data1);
+                  //showPage(data1);
+                
+                  };
+               };
    //   <div class="pagination">
-   //     <ul class="link-list">`    
+   //     <ul class="link-list">` 
+ 
+
    //return studentItem;
    //return showPage;
-   //return (data);
-      };
+   //return(data);
+
         
-  
-};
-//};
-showPage(data);
-  
-//console.log(data);
-  
-
-
 
 
 
@@ -79,3 +68,22 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+
+
+
+
+
+
+      //   li ="student-item cf";
+      //   let studentItem ="student-details" (`img class="avatar" src="js.data.js" alt="Profile, Picture`)
+      //   let h3 = "Name";
+      //   span_class="email";
+      //   div = "data";
+      //   div_class="joined-details";
+      //   span_lass="date";
+        //div
+        //li
+};
+
+//console.log(data);
+   
