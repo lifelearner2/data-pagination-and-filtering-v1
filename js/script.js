1852bf272e5eab68cf224180e7b1a274032c0931
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-06-07 15:51:50 -0700

    Update script.js
    fixed syntax

--- js/script.js
+++ js/script.js
@@ -29,12 +29,12 @@ function showPage(list, page) {
     if (i >= startIndex && i < endIndex) {
       const studentItem = `<li class="student-item cf">
       <div class="student-details">
-        <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-        <h3>${list[i].name.first}</h3>
-        <span class="email">${data[i].email}</span>
+        <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
+        <h3>${list[i].name.first} ${list[i].name.last}</h3>
+        <span class="email">${list[i].email}</span>
       </div>
       <div class="joined-details">
-        <span class="date">Joined 12-15-2005</span>
+        <span class="date">Joined ${list[i].registered.date}</span>
       </div>
     </li>`
 console.log(studentItem);
@@ -56,3 +56,4 @@ This function will create and insert/append the elements needed for the paginati
 
 console.log(data);
 
+//https://randomuser.me/api/portraits/women/25.jpg
