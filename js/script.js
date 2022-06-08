c3ce6e21f9ad777928931276d17c3a31befff28d
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-06-08 05:14:50 -0700

    Update script.js
    worked on second function

--- js/script.js
+++ js/script.js
@@ -23,9 +23,9 @@ function showPage(list, page) {
   //return showPage;
   //studentList.innerHTML = ('studentItem');
   studentList.innerHTML = "";
-  console.log("outside loop");
+  //console.log("outside loop");
   for (let i = 0; i < list.length; i++) {
-    console.log("inside loop");
+    //console.log("inside loop");
     if (i >= startIndex && i < endIndex) {
       const studentItem = `<li class="student-item cf">
       <div class="student-details">
@@ -37,13 +37,18 @@ function showPage(list, page) {
         <span class="date">Joined ${list[i].registered.date}</span>
       </div>
     </li>`
-console.log(studentItem);
+//console.log(studentItem);
       studentList.insertAdjacentHTML('beforeend', studentItem);
      
     }
   }
 }
 showPage(data, 1);
+// showPage(data, 2);
+// showPage(data, 3);
+// showPage(data, 4);
+// showPage(data, 5);
+
 //   <div class="pagination">
 //     <ul class="link-list">`
 
@@ -52,8 +57,63 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+      const numOfPages = 5;
+      const button = ("Next Page");
+
+      for (let i = 1; i <= numOfPages; i++) {
+            `<li>
+            <button type="button">1</button>
+            </li>
+               linkList.insertAdjacentHTML('beforeend', button);
+            <li>
+                <button type="firstButton" class="active">${button[i].firstPage}</button>
+            </li>
+            <li>
+                 <button type="button">${button[i].nextPage}</>
+            </li>` 
+               document.querySelector(".className")
+               numOfPages = Math.ceil(list.length, numOfPages ++, 9);{
+                  const linkList = document.querySelector(".link-list");
+                  document.getElementById("link-list").innerHTML("button");
+                  linkList.innerHTML = "";
+      }
+   
+         linkList.addEventListener("click", (e) => {
+            const nextPage =
+            `<li>
+            <button type="button" class="active">${button[i].firstButton}</button>
+            </li>
+            <li>
+            <button type="button">${button[i].nextPage}</>
+            </li>` 
+           
+            if (click.target.ul = button) {
+               document.querySelector(".className = active")
+               className = '';
+               document.querySelector(".className.click.target = active")
+               document.querySelector("remove active class from previous button")
+               showPage(list.textContent);
+               addPagination(data);
+               showPage(data, 1)
+               showPage(list, 1);
+            };
+        
+         //let btn = document.createElement("button");
+         //console.log(list);
+          });
+
+     
+            //removeEventListener()
+
+        };        
+      
+      };
+   
 // Call functions
 
-console.log(data);
+showPage(data, 1);
+//addPagination(data);
 
-//https://randomuser.me/api/portraits/women/25.jpg
+   
+console.log(data); 
